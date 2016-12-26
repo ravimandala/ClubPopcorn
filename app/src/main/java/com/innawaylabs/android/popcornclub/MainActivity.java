5dd56080c9bd051e77102961112a517ed78edf80
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2016-12-26 04:04:11 -0800

    Added menu options to toggle between top lists

--- app/src/main/java/com/innawaylabs/android/popcornclub/MainActivity.java
+++ app/src/main/java/com/innawaylabs/android/popcornclub/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<MoviesListItem> movies;
     private MoviesAdapter adapter;
     private final String QUERY_RESULTS = "results";
+    private int currentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,39 +43,69 @@ public class MainActivity extends AppCompatActivity {
 
         movies = new ArrayList<>();
         adapter = new MoviesAdapter(getApplicationContext(), movies);
-        fetchPopularMovies();
+        currentList = R.id.mi_top_rated_movies;
+        fetchTopMovies(getString(R.string.tmdb_top_rated_movies_suffix));
         rvMoviesList.setAdapter(adapter);
         rvMoviesList.setLayoutManager(
                 new GridLayoutManager(this,
                         getResources().getInteger(R.integer.movie_list_preview_columns)));
     }
 
-    // Send out the network request
-    private void fetchPopularMovies() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == currentList)
+            return super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.mi_popular_movies:
+                currentList = R.id.mi_popular_movies;
+                movies.clear();
+                fetchTopMovies(getString(R.string.tmdb_popular_movies_suffix));
+                return true;
+            case R.id.mi_top_rated_movies:
+                currentList = R.id.mi_top_rated_movies;
+                movies.clear();
+                fetchTopMovies(getString(R.string.tmdb_top_rated_movies_suffix));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void fetchTopMovies(String moviesApiSuffix) {
         AsyncHttpClient client = new AsyncHttpClient();
 
         RequestParams params = new RequestParams();
         params.add(getString(R.string.tmdb_key_api_key), getString(R.string.tmdb_api_v3_key));
-        client.get(getString(R.string.tmdb_api_v3_url_prefix) + getString(R.string.tmdb_popular_movies_suffix),
+        client.get(getString(R.string.tmdb_api_v3_url_prefix) + moviesApiSuffix,
                 params,
-                new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        try {
-                            movies.addAll(
-                                    MoviesListItem.createMoviesList(
-                                            response.getJSONArray(QUERY_RESULTS)));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        adapter.notifyDataSetChanged();
-                    }
+                new TopMoviesResponseHandler());
+    }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        Log.d(TAG, "Http request failed with statusCode = " + statusCode + " and error: " + throwable.getMessage());
-                        super.onFailure(statusCode, headers, responseString, throwable);
-                    }
-                });
+    private class TopMoviesResponseHandler extends JsonHttpResponseHandler {
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            try {
+                movies.addAll(
+                        MoviesListItem.createMoviesList(
+                                response.getJSONArray(QUERY_RESULTS)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+            Log.d(TAG, "Http request failed with statusCode = " + statusCode + " and error: " + throwable.getMessage());
+            super.onFailure(statusCode, headers, responseString, throwable);
+        }
     }
 }
